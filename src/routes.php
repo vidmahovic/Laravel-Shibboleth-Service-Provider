f7a4e1c14991731d599d687afe9288d05f9875ad
vidmahovic <vidmahovic@users.noreply.github.com>
2015-08-17 11:11:57 +0200

    Updated routes
    Updated routes according to my needs

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,10 @@
 <?php
 
 // Login Route (Shibboleth and Local)
-Route::get('/login', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@create');
+Route::get('/login', 'App\Http\Controllers\WelcomeController@login');
 // Logout Route (Shibboleth and Local)
-Route::get('/logout', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@destroy');
+Route::get('/logout', ['middleware' => 'auth', 'uses' =>'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@destroy']);
 // Shibboleth IdP Callback
-Route::get('/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@idpAuthorize');
+Route::get('/auth', ['middleware' => 'guest', 'uses' => 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@idpAuthorize']);
 
-// Login Route (Local)
-Route::get('/local', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@localCreate');
-// Login Callback (Local)
-Route::post('/local', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@localAuthorize');
-
-// Login Callback (Emulated)
-Route::get('emulated/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-// Login Callback (Emulated)
-Route::post('emulated/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-// Login Route (Emulated)
-Route::get('emulated/login', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
-// Logout Route (Emulated)
-Route::get('emulated/logout', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogout');
+?>
